7b2ae36c7dab7461a37c2f0b8b6cb881263f8f0d
caiocerqueirajs <caiocerqueira.silva@ucsal.edu.br>
2025-09-02 23:23:17 -0300

    Alterei DeviceTest.java

--- src/test/java/br/com/mariojp/solid/isp/DeviceTest.java
+++ src/test/java/br/com/mariojp/solid/isp/DeviceTest.java
@@ -4,10 +4,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DeviceTest {
+
     @Test
     void print_should_not_require_scan() {
-        var svc = new OfficeService(new SimplePrinter());
-        assertDoesNotThrow(() -> svc.printReport("Relatório"),
-                "Após refatoração, não deve chamar scan para imprimir");
+        OfficeService svc = new OfficeService(new SimplePrinter());
+
+        assertDoesNotThrow(
+                () -> svc.printReport("Relatório"),
+                "Após refatoração, não deve chamar scan para imprimir"
+        );
     }
 }
